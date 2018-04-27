4141a01e64898cbebfd51eaf6579f062bac237db
x77XDD <358190758@qq.com>
2018-04-27 16:07:09 +0800

    first submit code

---
+++ app/src/main/java/com/example/administrator/myapplication/MyAppInfo.java
@@ -0,0 +1,34 @@
+package com.example.administrator.myapplication;
+
+import android.graphics.drawable.Drawable;
+/**
+ * Created by gray_dog3 on 16/3/3.
+ */
+public class MyAppInfo {
+    private Drawable image;
+    private String appName;
+
+    public MyAppInfo(Drawable image, String appName) {
+        this.image = image;
+        this.appName = appName;
+    }
+    public MyAppInfo() {
+
+    }
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public void setImage(Drawable image) {
+        this.image = image;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+}
